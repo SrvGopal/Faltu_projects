70c62d86eb5eb44cf3c7dedbee5f4ac4771db232
GOPAL JEE (Advanced) <srvgopal07@gmail.com>
2024-05-25 14:11:25 +0530

    Update Question1.c

--- Question1.c
+++ Question1.c
@@ -1,1 +1,13 @@
 
+
+    printf("Enter the total days ");
+    scanf("%d",&days);
+
+    year = days / 365;
+    weeks = (days /7 )%52;
+    days = days / 365;
+
+    printf("It is %d Years: %d weeks: %d days",
+    year,weeks,days);
+
+}
