004df075abeb8a987cedf69891335d77ff1814c3
GOPAL JEE (Advanced) <srvgopal07@gmail.com>
2024-06-06 18:48:29 +0530

    Update password.py

--- password.py
+++ password.py
@@ -1,26 +1,53 @@
+from tkinter import *
 from random import *
 
-lst = []
-keys = "1234567890~!@#$%^&*()_+-`={}|[ABCDEGFHIJKLMNOPQRSTUVWXYZ]:;,/<>?.qwertyuiopasdfghjklzxcvbnm"
+root = Tk()
 
-for i in keys:
-    lst.append(i)
+root.geometry("500x500")
+root.title("Password Generator ")
 
-size = int(input("Enter the length of your password "))
-password =""
-length = len(lst)
+main_frame = Frame(root)
+main_frame.pack(pady=20, fill='x')
 
-for i in range(size+1):
-    index = randint(0,length)
-    char = lst[index]
-    password = password+char
+label_frame = Frame(main_frame)
+label_frame.pack(padx=10, pady=5)
 
-print(password)
+length_l = Label(label_frame, text="Password Length:", font=('Arial', 12))
+length_l.pack(side='left')
 
+length = IntVar()
+length_entry = Entry(label_frame, textvariable=length, width=5, font=('Arial', 12))
+length_entry.pack(side='left', padx=5)
 
+button_frame = Frame(main_frame)
+button_frame.pack(pady=10)
 
+submit_button = Button(button_frame, text="Generate Password",
+                       command=lambda: generate_password(),
+                       font=('Arial', 12))
 
+submit_button.pack(padx=10)
 
+password_frame = Frame(main_frame)
+password_frame.pack(pady=10, padx=10)
 
+password_label = Label(password_frame, text="Generated Password:", font=('Arial', 12))
+password_label.pack(side='left')
 
+password_output = Label(password_frame, text="", wraplength=400, font=('Arial', 12))
+password_output.pack(side='left', padx=5)
 
+def generate_password():
+    try:
+        size = length.get()
+        if size < 1:
+            password_output.config(text="Length must be at least 1")
+            return
+        keys = "1234567890~!@#$%^&*()_+-`={}|[ABCDEGFHIJKLMNOPQRSTUVWXYZ]:;,/<>?.qwertyuiopasdfghjklzxcvbnm"
+        lst = list(keys)
+        password = "".join(choice(lst) for _ in range(size))
+        password_output.config(text=password)
+    except:
+        password_output.config(text="Invalid input")
+
+root.mainloop()
